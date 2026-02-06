6b20e80e608ec20ac18ba9b40a2ba58270261ebc
Heitor Pires de Souza <heitorpsouza22@gmail.com>
2026-02-06 17:10:15 -0300

    Término segundo dia

--- src/main/java/br/eti/hpds/OSApiApplication/domain/model/Cliente.java
+++ src/main/java/br/eti/hpds/OSApiApplication/domain/model/Cliente.java
@@ -1,17 +1,45 @@
 
 package br.eti.hpds.OSApiApplication.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  *
  * @author Aluno
  */
+@Entity
 public class Cliente {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+    
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
+    
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "telefone")
     private String fone;
 
+    public Cliente() {
+    }
+
+    
+    
     public Cliente(long id, String nome, String email, String fone) {
         this.id = id;
         this.nome = nome;
