fced776f810415926e30e7d444bf30c84387ed3c
Heitor Pires de Souza <heitorpsouza22@gmail.com>
2026-02-24 13:55:04 -0300

    Acrescimo dos métodos get, put e delete

--- src/main/java/br/eti/hpds/OSApiApplication/api/controller/OrdemServicoController.java
+++ src/main/java/br/eti/hpds/OSApiApplication/api/controller/OrdemServicoController.java
@@ -1,10 +1,19 @@
 package br.eti.hpds.OSApiApplication.api.controller;
 
 import br.eti.hpds.OSApiApplication.domain.model.OrdemServico;
+import br.eti.hpds.OSApiApplication.domain.repository.OrdemServicoRepository;
 import br.eti.hpds.OSApiApplication.domain.service.OrdemServicoService;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,10 +26,49 @@ public class OrdemServicoController {
     @Autowired 
     private OrdemServicoService ordemServicoService;
     
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
+    
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrdemServico criar(@RequestBody OrdemServico ordemServico) {
         return ordemServicoService.criar(ordemServico);
     }
     
+    @GetMapping("/ordem-servico")
+    public List<OrdemServico> buscar(@PathVariable Long ordemServicoID) {
+        
+        Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoID);
+
+        if (ordemServico.isPresent()) {
+            return (List<OrdemServico>) ResponseEntity.ok(ordemServico.get());
+        } else {
+            return (List<OrdemServico>) ResponseEntity.notFound().build();
+        }
+    }
+
+    
+    @PutMapping("ordem-servico")
+    public ResponseEntity<OrdemServico> atualizar (@Valid @PathVariable Long ordemServicoID,
+                                                   @RequestBody OrdemServico ordemServico){
+        if(!ordemServicoRepository.existsById(ordemServicoID)){
+            return ResponseEntity.notFound().build();
+        }
+        
+        ordemServico.setId(ordemServicoID);
+        ordemServico = ordemServicoService.salvar(ordemServico);
+        return ResponseEntity.ok(ordemServico);
+    }
+    
+    @DeleteMapping("/ordem-servico")
+    public ResponseEntity<Void> excluir(@PathVariable Long clienteID){
+        
+        if(!ordemServicoRepository.existsById(clienteID)) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        ordemServicoService.excluir(clienteID);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/eti/hpds/OSApiApplication/domain/repository/OrdemServicoRepository.java
+++ src/main/java/br/eti/hpds/OSApiApplication/domain/repository/OrdemServicoRepository.java
@@ -1,8 +1,13 @@
 package br.eti.hpds.OSApiApplication.domain.repository;
 
 import br.eti.hpds.OSApiApplication.domain.model.OrdemServico;
+import java.math.BigDecimal;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrdemServicoRepository extends JpaRepository<OrdemServico , Long> {
     
+    List<OrdemServico> findByNome (String nome);
+    List<OrdemServico> findByDescricao (String descricao);
+    OrdemServico findByPreco (BigDecimal preco);
 }
