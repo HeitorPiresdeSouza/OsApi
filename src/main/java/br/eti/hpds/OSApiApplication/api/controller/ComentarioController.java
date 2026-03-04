fb26d4ecac434acef05d1e34bc45e9e955545a5b
Heitor Pires de Souza <heitorpsouza22@gmail.com>
2026-03-04 08:27:59 -0300

    Término do rascunho da classe ComentarioControllere ComentarioService

--- src/main/java/br/eti/hpds/OSApiApplication/api/controller/ComentarioController.java
+++ src/main/java/br/eti/hpds/OSApiApplication/api/controller/ComentarioController.java
@@ -1,8 +1,20 @@
 package br.eti.hpds.OSApiApplication.api.controller;
 
 import br.eti.hpds.OSApiApplication.domain.model.Comentario;
+import br.eti.hpds.OSApiApplication.domain.repository.ComentarioRepository;
+import br.eti.hpds.OSApiApplication.domain.repository.OrdemServicoRepository;
+import br.eti.hpds.OSApiApplication.domain.service.ComentarioService;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,10 +23,69 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/comentario")
 public class ComentarioController {
-    
+
+    @Autowired
+    private ComentarioRepository comentarioRepository;
+
+    @Autowired
+    private ComentarioService comentarioService;
+
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Comentario criar(@RequestBody Comentario comentario) {
         return comentario.criar(comentario);
     }
+
+    @GetMapping("/comentario/{comentarioID}")
+    public List<Comentario> buscarById(@PathVariable Long comentarioID) {
+
+        Optional<Comentario> comentario = comentarioRepository.findById(comentarioID);
+
+        if (comentario.isPresent()) {
+            return (List<Comentario>) ResponseEntity.ok(comentario.get());
+        } else {
+            return (List<Comentario>) ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/comentario")
+    public ResponseEntity<List<Comentario>> findTodos() {
+
+        List<Comentario> comentario = comentarioService.findTodosComentarios();
+
+        if (!comentario.isEmpty()) {
+            return ResponseEntity.ok(comentario);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PutMapping("/comentario/{comentarioID}")
+    public ResponseEntity<Comentario> atualizar (@Valid @PathVariable Long comentarioID,
+            @RequestBody Comentario comentario) {
+        if (!comentarioRepository.existsById(comentarioID)) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        comentario.setId(comentarioID);
+        comentario = comentarioService.salvar(comentario);
+        return ResponseEntity.ok(comentario);
+    }
+    
+    @DeleteMapping("/comentario/{comentarioID}")
+    public ResponseEntity<Void> excluir(@PathVariable Long comentarioID) {
+        
+        if (!comentarioRepository.existsById(comentarioID)) {
+            return ResponseEntity.notFound().build();
+        } else {
+            comentarioService.excluir(comentarioID);
+            return ResponseEntity.noContent().build();
+        }
+    }
 }
+/**
+ *
+ */
--- src/main/java/br/eti/hpds/OSApiApplication/domain/service/ComentarioService.java
+++ src/main/java/br/eti/hpds/OSApiApplication/domain/service/ComentarioService.java
@@ -1,11 +1,15 @@
 package br.eti.hpds.OSApiApplication.domain.service;
 
+import br.eti.hpds.OSApiApplication.domain.exception.DomainException;
 import br.eti.hpds.OSApiApplication.domain.model.Comentario;
+import br.eti.hpds.OSApiApplication.domain.model.OrdemServico;
 import br.eti.hpds.OSApiApplication.domain.model.StatusOrdemServico;
 import br.eti.hpds.OSApiApplication.domain.repository.ClienteRepository;
 import br.eti.hpds.OSApiApplication.domain.repository.ComentarioRepository;
 import br.eti.hpds.OSApiApplication.domain.repository.OrdemServicoRepository;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,4 +35,24 @@ public class ComentarioService {
     public void excluir(Long comentarioID){
         comentarioRepository.deleteById(comentarioID);
     }
+    
+     public List<Comentario> findTodosComentarios(){
+        
+        return comentarioRepository.findAll();
+        
+    }
+     
+     public Comentario salvar (Comentario comentario) {
+         Optional<OrdemServico> ordemServicoExistente = ordemServicoRepository.findById(comentario.getId());
+         
+         if(!ordemServicoExistente.isPresent()) {
+             throw new DomainException("Cliente não existe");
+         } else {
+             return comentarioRepository.save(comentario);
+         }
+     }
 }
+
+/**
+ *
+ */
